a32aa1cc92782ab5f3ca94f123223d862d1a4d0d
Roshan Kumar <ROSHAN.20211CSE0508@presidencyuniversity.in>
2024-02-16 09:24:41 +0530

    Add project files.

---
+++ FullStack1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace hello
+{
+class one
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the calue for A ");
+            int  a =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a);
+        }
+    }
+}
