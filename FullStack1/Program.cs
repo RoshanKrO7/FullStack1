f6b9b0274208fdbdfc1528288e4a380eb38a713c
Roshan Kumar <ROSHAN.20211CSE0508@presidencyuniversity.in>
2024-02-16 10:10:18 +0530

    This is upto Constructor LV and GV

--- FullStack1/Program.cs
+++ FullStack1/Program.cs
@@ -5,9 +5,44 @@ class one
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter the calue for A ");
-            int  a =Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(a);
+            /** Console.WriteLine("Enter the value for A ");
+             int  a =Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine(a);
+             Console.WriteLine("Enter a String ");
+             string s = Console.ReadLine();
+             Console.WriteLine(s);
+             person p1 =new person();
+             Console.WriteLine(p1.a);
+             p1.display();**/
+            college c1 = new college(1234,"Presidency University");
+            college c2 = new college();
+            Console.WriteLine(c1.cid);
+            Console.WriteLine(c2.name);
+
+        }
+    }
+    /**class person
+    {
+        public int a = 10;
+        public void display()
+        {
+            Console.WriteLine("I am the display Function");
+        }
+    }**/
+    class college
+    {
+        public int cid;//Global Variable
+        public string name;
+
+        public college()//For default Constructor 
+        {
+            Console.WriteLine("I am in the Default Constructor");
+        }
+
+        public college(int cid1, string name1)//Local Variable
+        {
+            this.cid = cid1;//mapping of LV to GV
+            this.name = name1;
         }
     }
 }
