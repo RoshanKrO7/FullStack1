ddd2246b5705970ff6165b0f8835e673bad2c152
Roshan Kumar <ROSHAN.20211CSE0508@presidencyuniversity.in>
2024-02-16 10:31:32 +0530

    Final 1st Class

--- FullStack1/Program.cs
+++ FullStack1/Program.cs
@@ -13,11 +13,14 @@ class one
              Console.WriteLine(s);
              person p1 =new person();
              Console.WriteLine(p1.a);
-             p1.display();**/
+             p1.display();
             college c1 = new college(1234,"Presidency University");
             college c2 = new college();
             Console.WriteLine(c1.cid);
-            Console.WriteLine(c2.name);
+            Console.WriteLine(c2.name);**/
+            Console.WriteLine(bank.ifsc_code);
+            Console.WriteLine(bank.branch_name);
+            bank.timings();
 
         }
     }
@@ -29,7 +32,7 @@ class one
             Console.WriteLine("I am the display Function");
         }
     }**/
-    class college
+    /**class college
     {
         public int cid;//Global Variable
         public string name;
@@ -43,6 +46,15 @@ class one
         {
             this.cid = cid1;//mapping of LV to GV
             this.name = name1;
+        }
+    }**/
+    class bank
+    {
+        static public int ifsc_code = 12345678;
+        static public string branch_name = "pune";
+        static public void timings()
+        {
+            Console.WriteLine("The Banking timing is 10:30 to 3:30");
         }
     }
 }
