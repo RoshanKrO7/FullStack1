57959a9e4431e4c5f0bc471de5d58e871f2ab4de
Roshan Kumar <159796530+RoshanKrO7@users.noreply.github.com>
2024-03-01 09:45:04 +0530

    Create Collections.cs

---
+++ Lab3/Collections.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.generic;
+
+public class One
+{
+    public static void Main(string[] args)
+    {
+        List<string> names = new List<string>();
+        names.Add("RamKrishna");
+        names.Add("Swarnim");
+        names.Add("Sayeed");
+        names.Add("Roshan");
+        names.Add("Yashica");
+        names.Add("Deepshikha");
+        names.Add("Thushani");
+        names.Add("Hema");
+        names.Add("Shruti");
+        foreach (var i in names)
+        {
+            Console.WriteLine(i);
+        }
+        Dictionary<string, int> ages = new Dictionary<string, int>();
+        ages.Add("Ramkrishna", 21);
+        ages.Add("Swarnim", 21);
+        ages.Add("Sayeed", 22);
+        ages.Add("Roshan", 22);
+        ages.Add("Yashica", 21);
+        ages.Add("Deepshiskha", 21);
+        foreach (var k in ages)
+        {
+            Console.WriteLine(k);
+        }
+        Hastable table1=new Hastable();
+        table1.add(1, "abc");
+        table1.add("def", 2);
+        table1.add(3, 4);
+        Console.WriteLine("Key and Value pairs from table1");
+        foreach(var i in table1)
+        {
+            Console.Write(i);
+        }
+    }
+    
+}
